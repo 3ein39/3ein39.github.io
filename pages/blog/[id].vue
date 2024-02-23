70539c88332c24a89667b905dfcbd8c456de018a
Hussein Hany <hussein.hany.cs@gmail.com>
2024-02-23 14:16:50 +0200

    Add new pages and templates

---
+++ pages/blog/[id].vue
@@ -0,0 +1,11 @@
+<script setup>
+const route = useRoute()
+
+</script>
+
+<template>
+    <div>
+        <h1>Blog Post</h1>
+        <p>This is a blog post {{ route.params.id }}</p>
+    </div>
+</template>
---
+++ pages/blog/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Blog Post</h1>
+        <p>This is all blog posts</p>
+    </div>
+</template>
